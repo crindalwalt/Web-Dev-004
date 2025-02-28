808a9c9cf566203e5b948dc8c9eeb41ab1a3c7c0
mishalwaqar <mishalwaqar26@gmail.com>
2025-02-28 10:56:50 +0500

    Assignmnt2_(MW)

---
+++ assignments/02_Mishal.js
@@ -0,0 +1,57 @@
+// Operators
+
+// Arthmetic Operators
+//Addition
+
+let num1 = 55;
+let num2 = 10;
+console.log(num1 + num2); // number addition
+
+// string concatenation
+
+let str1= " Hello";
+let str2 = " world";
+console.log(str1+str2);
+
+//others
+ let result = num1-num2;
+ result= num1*num2;
+ result = num1/num2;
+ result= num1%num2;
+  console.log(result);
+
+  // comparision operator
+
+  let a = 67;
+  let b = 10;
+  lock = a > b;
+  lock = b<a;
+  lock = a<b;
+  lock = a<=b;
+  lock = a>=b;
+  console.log(lock);
+
+
+  let new1= 665;
+  let new2= "676";
+  equality = new1 == new2;
+  equality = new1 !=new2;
+  equality = new1 === new2;
+  console.log(equality);
+
+  // Assignment Operator
+
+  let initial =80;
+  initial += 56;
+  initial -= 10;
+  initial *= 5;
+  initial /= 2;
+  console.log(initial);
+
+  // logical operator
+
+  let x= 5;
+  let y= 10
+  console.log(x > y || y > x);
+  console.log(x > y && x > y);
+  
