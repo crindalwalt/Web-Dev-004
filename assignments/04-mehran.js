3754b16aaa844ff544cb4f298c5fe3a5cbe5f302
mishalwaqar <mishalwaqar26@gmail.com>
2025-03-06 21:26:28 +0500

    html basics 01

---
+++ assignments/04-mehran.js
@@ -0,0 +1,14 @@
+
+let nam = "Awais";
+
+switch (nam) {
+    case "Riaz":
+        console.log("Thanks");
+        break;
+    case "Talha":
+        console.log("Riaz is Your Brother?")
+        break;
+    case "Awais":
+        console.log("Done")
+        break;
+}
---
+++ assignments/Sheraz.js
@@ -0,0 +1,20 @@
+/**
+ * OPERATORS
+ * 4 types of operators
+ * 
+ *  1    Arithmatic operator [+,-,*,/,%,a++,++a ]✅
+ *  2   Camparision operator [<,>,<=,>=,==,===,!=,]✅
+ *  4    Assignment operator [=,+=,-=,/=,*=,%=] ✅
+ *  3    Logical operator   [&& , ||, !] ✅
+ * 
+ */
+let d = 50;
+let e = 20;
+* // Arithmatic operator [/,*,+,-,%, a++, ++a]
+
+let social = d + e;
+social = d - e;
+social = d / e;
+social = d * e;
+social = d % e;
+console.log(social)
