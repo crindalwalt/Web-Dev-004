aec2b857d1885712cd131ec5c9c5971c26c40916
mishalwaqar <mishalwaqar26@gmail.com>
2025-03-18 21:57:07 +0500

    Assignment_06(MW)

---
+++ assignments/06_Mishal.js
@@ -0,0 +1,20 @@
+
+// hexadecimal range from 0 to 9 and A to F
+
+function hexToBinary(hex) {  // function declaration
+    return parseInt(hex, 16);
+}
+
+// Calling the function
+
+
+let binaryValue = hexToBinary("1A3");
+console.log(binaryValue);
+
+
+
+// example 
+
+let result = hexToBinary("A32");
+console.log(result);
+
